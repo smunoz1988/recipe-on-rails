e79b174a3802e47ddbb38792135429f7d0378c1f
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-01 22:23:36 -0500

    add new, create and destroy methods to food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,19 @@ class FoodsController < ApplicationController
   def index
     @user = User.includes(:foods).find(params[:user_id])
   end
+
+  def new
+    @user = User.find(params[:user_id]) 
+    @food = Food.new
+  end
+
+  def create
+    @user = current_user
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to user_foods_path
+  end
 end
