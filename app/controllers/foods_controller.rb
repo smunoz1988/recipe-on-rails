8b86ddf1ce2ba76f9794a9df81f71601ab4f95ea
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-01 22:46:58 -0500

    add create method for foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,17 +4,26 @@ class FoodsController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id]) 
     @food = Food.new
   end
 
   def create
-    @user = current_user
+    @food = Food.new(post_params)
+    @food.user_id = current_user.id
+    if @food.save
+      redirect_to user_foods_path
+    else
+      render :new
+    end
   end
 
   def destroy
     @food = Food.find(params[:id])
     @food.destroy
     redirect_to user_foods_path
+  end
+
+  def post_params
+    params.require(:food).permit(:name, :measurement_unit,:price, :quantity)
   end
 end
