7f00993e9549a269a2521e51b540b25822e0fbe1
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-02 22:08:42 -0500

    create index on publicrecipes controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,2 +1,5 @@
 class PublicRecipesController < ApplicationController
+    def index
+    #  render all recipes and users names with a public status
+    @public_recipes = Recipe.where(public: true)
 end
