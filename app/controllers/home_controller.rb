f9f5277d54f5da2b7e3c76c1bc4c65e5fa0d0d88
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-04 13:46:34 -0500

    build shopping list logic on user model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @shopping_list = current_user.shopping_list
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :recipes, foreign_key: 'user_id'
   has_many :foods, foreign_key: 'user_id'
+
+  def shopping_list
+    # Get all the recipe foods for the current user's recipes
+    recipe_foods = RecipeFood.joins(:recipe).where(recipes: { user_id: self.id })
+
+    # Group the recipe foods by their associated food items and calculate the total quantity required for each food item
+    required_foods = recipe_foods.group(:food_id).sum(:quantity)
+
+    # Create a hash to store the shopping list
+    shopping_list = {}
+
+    # Compare the total quantity of each food item required with the available quantity in the user's foods
+    required_foods.each do |food_id, total_quantity_required|
+      food = Food.find(food_id)
+      available_quantity = food.quantity || 0
+      quantity_to_buy = [0, total_quantity_required - available_quantity].max
+      shopping_list[food] = quantity_to_buy if quantity_to_buy > 0
+    end
+
+    shopping_list
+  end
 end
