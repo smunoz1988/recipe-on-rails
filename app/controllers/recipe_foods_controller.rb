eb5106d58cd7537676be9c5eafbe3031f9351e1b
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-04 13:12:09 -0500

    add edit and update

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -15,9 +15,24 @@ class RecipeFoodsController < ApplicationController
   end
 
   def destroy
-    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food = RecipeFood.includes(:food).find(params[:id])
     @recipe_food.destroy
     redirect_to user_recipe_path(user_id: current_user.id, id: @recipe_food.recipe_id)
+  end
+
+  def edit
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+  
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_id = params[:recipe_id]
+    if @recipe_food.update(post_params)
+      redirect_to user_recipe_path(user_id: current_user.id, id: @recipe_food.recipe_id) , notice: 'The ingredient was successfully updated.'
+    else
+      redirect_to user_recipe_path(user_id: current_user.id, id: @recipe_food.recipe_id), alert: 'The ingredient could not be updated.'
+    end
   end
 
   def post_params
