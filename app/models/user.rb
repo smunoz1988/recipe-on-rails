1b25b3b9b7a9558baebdfff78b2ff50224c89249
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-04 14:10:36 -0500

    add method to get shoopping list method

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,27 @@ class User < ApplicationRecord
     # Create a hash to store the shopping list
     shopping_list = {}
 
+    # Initialize total items and total value variables
+    total_items = 0
+    total_value = 0
+
     # Compare the total quantity of each food item required with the available quantity in the user's foods
     required_foods.each do |food_id, total_quantity_required|
       food = Food.find(food_id)
       available_quantity = food.quantity || 0
       quantity_to_buy = [0, total_quantity_required - available_quantity].max
-      shopping_list[food] = quantity_to_buy if quantity_to_buy > 0
+
+      if quantity_to_buy > 0
+        # Calculate the total price for the required quantity
+        total_price = food.price * quantity_to_buy
+        shopping_list[food] = { quantity: quantity_to_buy, total_price: total_price }
+
+        # Increment total items and total value
+        total_items += quantity_to_buy
+        total_value += total_price
+      end
     end
 
-    shopping_list
+    { shopping_list: shopping_list, total_items: total_items, total_value: total_value }
   end
 end
