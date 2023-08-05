bf4b1ba456ebc001f95c1161e47546013eb35b17
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-05 11:02:09 -0500

    update unused endpoints on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   resources :home, only: [:index, :show]
   resources :users do
-    resources :foods
+    resources :foods, only: [:index, :new, :create, :edit, :update, :destroy]
     resources :recipes do
       member do
         patch 'toggle'
       end
-      resources :recipe_foods
+      resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
     end
-    resources :public_recipes
+    resources :public_recipes, only: [:index]
   end
   root 'home#index'
 end
